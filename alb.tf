9087425737944f7a3527a84510584372b749f07d
Shalini Kallepalli <51354004+Shalini2131@users.noreply.github.com>
2025-01-24 14:42:07 -0600

    Created alb.tf

---
+++ alb.tf
@@ -0,0 +1,44 @@
+# Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public.id]
+
+  tags = {
+    Name = "ApplicationLoadBalancer"
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "tg" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "TargetGroup"
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
