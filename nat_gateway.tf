ee713896d5427f98131beade814adbf04ca19f1d
Shalini Kallepalli <51354004+Shalini2131@users.noreply.github.com>
2025-01-24 14:45:27 -0600

    Created nat_gateway.tf

---
+++ nat_gateway.tf
@@ -0,0 +1,34 @@
+# NAT Gateway for Internet Access from private subnet
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = {
+    Name = "NATGatewayEIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "NATGateway"
+  }
+}
+
+# Route Table for Private Subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
